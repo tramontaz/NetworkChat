8925e4a14159226c8647557b55274a822206cdf7
tramontaz <v.chemodurov@yandex.ru>
2017-03-16 10:21:11 +0300

    Create Client window

--- src/chat/client/ClientGUI.java
+++ src/chat/client/ClientGUI.java
@@ -1,7 +1,32 @@
 package chat.client;
 
+import javax.swing.*;
+
 /**
  * Created by Администратор on 16.03.2017.
  */
-public class ClientGUI {
+public class ClientGUI extends JFrame {
+
+    private static final int WINDOW_WIDTH = 400;
+    private static final int WINDOW_HEIGHT = 300;
+    private static final String WINDOW_TITLE = "Network chat";
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+
+    }
+
+    private ClientGUI(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setTitle(WINDOW_TITLE);
+        setVisible(true);
+    }
 }
